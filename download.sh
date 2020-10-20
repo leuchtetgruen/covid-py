a24885d2ffd93f8f3b41de4adfdb2ac6081a366e
hannes <hannes@leuchtetgruen.de>
2020-10-20 10:30:58 +0200

    download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+rm confirmed.csv & wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O confirmed.csv
+rm deaths.csv & wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O deaths.csv
+rm rki.csv & wget https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv -O rki.csv
